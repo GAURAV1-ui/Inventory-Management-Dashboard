ce5491a432747fdfa4c00e31c688d58127596be7
Gaurav <iamgaurav201@gmail.com>
2024-08-13 21:53:53 +0530

    Updated

---
+++ client/src/app/(components)/index.tsx
@@ -0,0 +1,16 @@
+"use-client";
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className={`flex justify-between items-center w-full mb-7`}>
+        <div className='flex justify-between items-center gap-5'>
+            <button>
+
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
